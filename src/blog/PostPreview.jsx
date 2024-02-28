540e5fbf655ce44cc9b19c1e6e7f921a54d6a19e
Michael Kushman <michaelkushman@MacBook-Pro.local>
2024-02-28 00:20:41 -0800

    fixed post links missing slug from blog home

--- src/blog/PostPreview.jsx
+++ src/blog/PostPreview.jsx
@@ -5,6 +5,7 @@ import dayjs from 'dayjs'
 
 const PostPreview = ({postId, titlePlaintext, username, bodyPlaintext, createdAt, numComments, authorImageUrl, slug}) => {
     const date = dayjs(createdAt).format('MMM DD, YYYY')
+    let postUri = slug+"/"+postId
 
     let preview = ''
     if(bodyPlaintext){
@@ -15,7 +16,7 @@ const PostPreview = ({postId, titlePlaintext, username, bodyPlaintext, createdAt
     
     return (
     <Box sx={{p:4, t:2,maxWidth:700}}>
-      <Link color='inherit' variant='h4' underline="none" href={slug+"/"+postId}>{titlePlaintext}</Link>
+      <Link color='inherit' variant='h4' underline="none" href={postUri}>{titlePlaintext}</Link>
       <Stack direction="row" spacing={3} sx={{p:3}} >
       
         <Avatar src={authorImageUrl}/>
@@ -23,9 +24,9 @@ const PostPreview = ({postId, titlePlaintext, username, bodyPlaintext, createdAt
         <Typography>{date}</Typography>
         </Stack>
 
-        <Box p={4}>{preview}{<Link href={slug+"/"+postId} color='inherit' underline='none'>  ...read more</Link>}</Box>
+        <Box p={4}>{preview}{<Link href={postUri} color='inherit' underline='none'>  ...read more</Link>}</Box>
         <Stack direction="row" spacing={3} sx={{p:3}} >
-          <Button color='inherit' href={`${postId}#comments`}>{numComments} {numComments == 1 ? "Comment": "Comments"}</Button>
+          <Button color='inherit' href={`${postUri}#comments`}>{numComments} {numComments == 1 ? "Comment": "Comments"}</Button>
 
         </Stack>
 
