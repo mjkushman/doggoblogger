e1e097a521d033c90990c2526674467395bb3400
Michael Kushman <michaelkushman@MacBook-Pro.local>
2024-03-27 15:24:35 -0700

    removed unused import

--- src/common/Hero.jsx
+++ src/common/Hero.jsx
@@ -1,4 +1,4 @@
-import { Box, Container, CssBaseline, Typography } from "@mui/material";
+import { Box, Container, Typography } from "@mui/material";
 
 const Hero = ({ headline, size, backgroundImage }) => {
   if (backgroundImage == null) {
